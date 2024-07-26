9565fc4f07f545af1eaf60af8147c96d22a42550
Andres <andresm@leadboxhq.com>
2024-07-26 19:20:42 -0400

    new base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "andresmarquez02.github.io",
   plugins: [react()],
 })
