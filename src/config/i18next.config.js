af941888de704a9f1f4610a89ca4cc067666d16e
Andres <andresm@leadboxhq.com>
2024-08-15 09:53:28 -0400

    old portfolio

--- src/config/i18next.config.js
+++
@@ -1,7 +0,0 @@
-import i18next from "i18next";
-import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
-
-i18next.use(Backend).use(initReactI18next).init({
-    fallbackLng: "es",
-})
